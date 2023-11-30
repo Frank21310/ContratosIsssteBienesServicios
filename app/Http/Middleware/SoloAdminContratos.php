40a995d099007aeb5561e89a6e356e9cb4a27f0b
Francisco Daniel Santaella Ruiz <45299753+Frank21310@users.noreply.github.com>
2023-11-30 10:54:33 -0600

    New middleware auth

---
+++ app/Http/Middleware/SoloAdminContratos.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+
+class SoloAdminContratos
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        switch(auth::user()->rol_id){
+            case ('1'):
+                return redirect('Administrador');//Administrador
+            break;
+            case ('2'):
+                return redirect('Peticiones');//Requirente
+            break;
+            case ('3'):
+                return redirect('Contratante');//Contratante
+            break;
+            case ('4'):
+                return $next($request);//AdministradorContratos
+            break;
+            case ('5'):
+                return redirect('Finanzas');//Finanzas
+            break;
+            case ('6'):
+                return redirect('AreaNormativa');//Area Normativa
+            break;
+        }
+    }
+}
