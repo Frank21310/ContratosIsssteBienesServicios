224c905d6f65ab92271eabd57dc509dcee98434e
Francisco Daniel Santaella Ruiz <45299753+Frank21310@users.noreply.github.com>
2023-11-30 10:58:09 -0600

    middleware info

--- app/Http/Middleware/SoloAdministrador.php
+++ app/Http/Middleware/SoloAdministrador.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
 
 class SoloAdministrador
 {
@@ -15,6 +17,25 @@ class SoloAdministrador
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        switch(auth::user()->rol_id){
+            case ('1'):
+                return $next($request);//Administrador
+            break;
+            case ('2'):
+                return redirect('Peticiones');//Requirente
+            break;
+            case ('3'):
+                return redirect('Contratante');//Contratante
+            break;
+            case ('4'):
+                return redirect('AdminContratos');//AdministradorContratos
+            break;
+            case ('5'):
+                return redirect('Finanzas');//Finanzas
+            break;
+            case ('6'):
+                return redirect('AreaNormativa');//Area Normativa
+            break;
+        } 
     }
 }
